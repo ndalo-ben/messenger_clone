4dbeb40db0fc7dfa818630b538c1ca9d2f6cd325
ben <ndaloben8@gmail.com>
2023-12-09 10:07:00 +0300

    updates

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
-import React from 'react'
 
 export default function Home() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image src={"/images/logo.png"} alt='' width={50} height={50} className=''/>
+        <Image
+          src={"/images/logo.png"}
+          alt='messenger'
+          width={50}
+          height={50}
+          className=''
+        />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-grey-900">
           Sign in to your account
         </h2>
@@ -13,4 +18,3 @@ export default function Home() {
     </div>
   )
 }
- 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Messenger Clone',
@@ -16,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
