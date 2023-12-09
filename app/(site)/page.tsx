0438f0c9543ff127f1183279cbc5f7105126b08f
ben <ndaloben8@gmail.com>
2023-12-09 13:40:36 +0300

    logo update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
-          src={"/images/logo.png"}
+          src={"/logo.png"}
           alt='messenger'
           width={50}
           height={50}
+          quality={100}
+          sizes='lg'
           className=''
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-grey-900">
